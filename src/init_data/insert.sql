c109d70df4e1ffdc1e8a2f20cb73859cf57fb32c
Liam Masias <liammasias2@gmail.com>
2023-11-16 13:12:35 -0700

    Updated user table to fix error

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (username, firstName, lastName, password) VALUES
-("LembaMati", "Liam", "Masias", 'HelloWorld!'),
-("gamerNerd", "Charlie", "Daniels", "game4Ever?"),
-("lol6007", "Noah", "Smith", "lol6007");
+('LembaMati', 'Liam', 'Masias', 'HelloWorld!'),
+('gamerNerd', 'Charlie', 'Daniels', 'game4Ever?'),
+('lol6007', 'Noah', 'Smith', 'lol6007');

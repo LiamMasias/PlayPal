8dd1a3559c6c132b6b283288346f940d8b8f9d7a
Liam Masias <liammasias2@gmail.com>
2023-11-16 15:26:59 -0700

    Fixing database error

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -1,5 +1,5 @@
 -- Add emails to username database
 INSERT INTO users (username, email, firstName, lastName, password) VALUES
-('LembaMati', 'liam@yay.com' 'Liam', 'Masias', 'HelloWorld!'),
+('LembaMati', 'liam@yay.com', 'Liam', 'Masias', 'HelloWorld!'),
 ('gamerNerd', 'gamernerd@gmail.com', 'Charlie', 'Daniels', 'game4Ever?'),
 ('lol6007', 'noahspony@xbox.com', 'Noah', 'Smith', 'lol6007');

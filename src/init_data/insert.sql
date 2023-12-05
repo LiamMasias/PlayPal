b6db8b64c18b4e750ba101f011c73fe67bb37a2f
dezy-x2 <df.dezmond9@gmail.com>
2023-12-04 20:02:49 -0700

    updated target links and added more reviews

--- src/init_data/insert.sql
+++ src/init_data/insert.sql
@@ -6,5 +6,8 @@ INSERT INTO users (username, email, firstName, lastName, password) VALUES
 INSERT INTO reviews (gameId, userId, userName, rating, reviewText) VALUES
 (191406, 1, 'LembaMati', 5, 'This game is awesome!'),
 (191406, 2, 'gamerNerd', 4, 'This game is pretty good.'),
-(191406, 3, 'lol6007', 3, 'This game is okay.');
+(191406, 3, 'lol6007', 3, 'This game is okay.'),
+(119133, 1, 'LembaMati', 5, 'This game is awesome!'),
+(119133, 2, 'gamerNerd', 4, 'This game is pretty good.'),
+(119133, 3, 'lol6007', 1, 'Absolute GARBAGE!!!');
 
